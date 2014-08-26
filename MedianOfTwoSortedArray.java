1797aaf4f8f0f6dc1a0bb0c70d477b96165c21e5
Altoids <altoids2014@outlook.com>
2014-08-25 22:23:09 -0700

    medianoftwosortedarrays and its test case
    passed oj

--- MedianOfTwoSortedArray.java
+++ MedianOfTwoSortedArray.java
@@ -1,5 +1,5 @@
 /*There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
- * 
+ * http://fisherlei.blogspot.com/2012/12/leetcode-median-of-two-sorted-arrays.html
  */
 
 import java.util.*;
@@ -7,8 +7,35 @@ import java.util.*;
 public class MedianOfTwoSortedArray {
 
 	public double findMedianSortedArrays(int A[], int B[]) {
-        
+		int m = A.length;
+		int n = B.length;
+        if ((m + n) % 2 == 0)
+        	return (getMedian(A, B, (m+n)/2) + getMedian(A, B, (m+n)/2+1))/2;
+        else
+        	return getMedian(A,B, (m+n)/2 + 1);
     }
+	
+	private double getMedian(int a[], int b[], int k){
+		int m = a.length;
+		int n = b.length;
+		
+		if (m <= 0) return b[k-1];
+		if (n <= 0) return a[k-1];
+		if (k <= 1) return Math.min(a[0], b[0]);
+		
+		if (m/2 + n/2 + 1 >= k){
+			if (a[m/2] >= b[n/2]) // a 2 should be removed
+				return getMedian(Arrays.copyOfRange(a, 0, m/2), b, k);
+			else
+				return getMedian(a, Arrays.copyOfRange(b, 0, n/2), k );
+		}else{
+			if (a[m/2] >= b[n/2]) // b 1 should be removed
+				return getMedian(a, Arrays.copyOfRange(b, n/2+1, n), k- n/2-1);
+			else
+				return getMedian(Arrays.copyOfRange(a,m/2+1, m), b, k- m/2-1);
+		}
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sIn = new Scanner(System.in);
@@ -23,6 +50,9 @@ public class MedianOfTwoSortedArray {
 		for (int i = 0; i< blen; i++){
 			b[i] = sIn.nextInt();
 		}
+		
+		MedianOfTwoSortedArray m = new MedianOfTwoSortedArray();
+		System.out.println(m.findMedianSortedArrays(a, b));
 
 	}
 
