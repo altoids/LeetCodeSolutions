fdeefac79403952249cb92ca3b2578c827135850
Altoids <altoids2014@outlook.com>
2014-08-30 09:18:48 -0700

    convertsortedlisttobinarysearchtree template

---
+++ ConvertSortedListtoBinarySearchTree.java
@@ -0,0 +1,27 @@
+/*
+ * Convert Sorted List to Binary Search Tree Total Accepted: 17517 Total Submissions: 64305 My Submissions
+Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+public class ConvertSortedListtoBinarySearchTree {
+	public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode(int x) { val = x; next = null; }
+		  }
+	
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+	
+	public TreeNode sortedListToBST(ListNode head) {
+        
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
