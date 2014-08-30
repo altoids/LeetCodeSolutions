19ccc685ad723b125438ef01310c22cb2e91ce2f
Altoids <altoids2014@outlook.com>
2014-08-30 09:12:21 -0700

    change file name for binary tree maxmum path sum

---
+++ BinaryTreeMaximumPathSum.java
@@ -0,0 +1,32 @@
+/*
+ * Binary Tree Maximum Path Sum Total Accepted: 17353 Total Submissions: 86764 My Submissions
+Given a binary tree, find the maximum path sum.
+
+The path may start and end at any node in the tree.
+
+For example:
+Given the below binary tree,
+
+       1
+      / \
+     2   3
+Return 6.
+ */
+import java.util.*;
+
+public class BinaryTreeMaximumPathSum {
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+	public int maxPathSum(TreeNode root) {
+        
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
