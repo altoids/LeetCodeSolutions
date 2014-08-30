8aff908b2285c4c8f0a6f71390ffc76f4ca62b78
Altoids <altoids2014@outlook.com>
2014-08-30 09:16:29 -0700

    convertsortedarraytobinarysearchtree template

---
+++ ConvertSortedArraytoBinarySearchTree.java
@@ -0,0 +1,21 @@
+/*
+ * Convert Sorted Array to Binary Search Tree Total Accepted: 19009 Total Submissions: 58267 My Submissions
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+ */
+
+public class ConvertSortedArraytoBinarySearchTree {
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+	public TreeNode sortedArrayToBST(int[] num) {
+        
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
