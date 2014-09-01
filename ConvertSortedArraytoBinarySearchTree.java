36f0492ac2e120e907c813ba7c217792bb808086
Altoids <Altoids2014@outlook.com>
2014-09-01 15:11:54 -0700

    pre OJ
    convertsortedarraytobst and convertsortedlisttobst. havnen't tried oj
    yet

--- ConvertSortedArraytoBinarySearchTree.java
+++ ConvertSortedArraytoBinarySearchTree.java
@@ -3,19 +3,51 @@
 Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
  */
 
+import java.util.*;
 public class ConvertSortedArraytoBinarySearchTree {
 	public class TreeNode {
 		      int val;
 		      TreeNode left;
 		      TreeNode right;
 		      TreeNode(int x) { val = x; }
+		      
 		  }
 	public TreeNode sortedArrayToBST(int[] num) {
-        
+		//sorted array, pick from middle for root.
+		if (num == null || num.length == 0)
+			return null;
+		int rootindex = num.length/2;
+		TreeNode root = new TreeNode(num[rootindex]);
+		root.left = sortedArrayToBST(Arrays.copyOfRange(num, 0, rootindex));
+		root.right = sortedArrayToBST(Arrays.copyOfRange(num, rootindex +1, num.length));
+        return root;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sIn = new Scanner(System.in);
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		while(sIn.hasNext()){
+			al.add(sIn.nextInt());
+		}
+		int[] num = new int[al.size()];
+		for (int i = 0; i< al.size(); i++){
+			num[i] = al.get(i);
+		}
+		Arrays.sort(num);
+		/*for (int i = 0; i < num.length; i++)
+			System.out.print(num[i] + " ");
+		System.out.println("");*/
+		ConvertSortedArraytoBinarySearchTree c = new ConvertSortedArraytoBinarySearchTree();
+		printTree(c.sortedArrayToBST(num));
+		
+	}
+	private static void printTree(TreeNode root){
+		//inordertravel, it should print a sorted number list
+		if (root == null)
+			return;
+		printTree(root.left);
+		System.out.print(root.val + " ");
+		printTree(root.right);
 	}
 
 }
--- ConvertSortedListtoBinarySearchTree.java
+++ ConvertSortedListtoBinarySearchTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /*
  * Convert Sorted List to Binary Search Tree Total Accepted: 17517 Total Submissions: 64305 My Submissions
 Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
@@ -17,11 +19,64 @@ public class ConvertSortedListtoBinarySearchTree {
 		  }
 	
 	public TreeNode sortedListToBST(ListNode head) {
+        if (head == null)
+        	return null;
+        // find mid point of the sorted list
+        ListNode slow = head;
+        ListNode fast = head.next;
+        ListNode prevslow = slow;
+        while (slow != null && fast != null){
+        	prevslow = slow;
+        	slow = slow.next;
+        	fast = fast.next;
+        	if (fast != null)
+        		fast = fast.next;
+        }
         
+        // make mid point the root of this subtree
+        TreeNode root = new TreeNode(slow.val);
+        
+        //seperate left part of the list and call recursively, assign the return to left tree
+        if (prevslow == slow)
+        	head = null;
+        else
+        	prevslow.next = null;
+        ListNode rightlist = slow.next;
+        ListNode leftlist = head;
+        
+        root.left = sortedListToBST(leftlist);
+        //seperate right part of the list and call recursively, assign the return to right tree
+        root.right = sortedListToBST(rightlist);
+        // return root node
+        return root;
     }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sIn = new Scanner(System.in);
+		ConvertSortedListtoBinarySearchTree c = new ConvertSortedListtoBinarySearchTree();
+		ListNode head = null;
+		ListNode cur = null;
+		while(sIn.hasNext()){
+			int newNum = sIn.nextInt();
+			if (head == null){
+				//first node
+				head = c.new ListNode(newNum);
+				cur = head;
+			}else{
+				cur.next = c.new ListNode(newNum);
+				cur = cur.next;
+			}
+			cur.next = null;
+		}
+		printTree(c.sortedListToBST(head));
+	}
+	private static void printTree(TreeNode root){
+		//inordertravel, it should print a sorted number list
+		if (root == null)
+			return;
+		printTree(root.left);
+		System.out.print(root.val + " ");
+		printTree(root.right);
 	}
 
 }
