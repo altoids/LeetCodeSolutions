0176fc96d61e00dc7920bde7f9b53fab5fd95885
Altoids <Altoids2014@outlook.com>
2014-09-01 19:04:52 -0700

    pre oj for maxpathsum
    taking care of the leave is the biggest alone case

--- BinaryTreeMaximumPathSum.java
+++ BinaryTreeMaximumPathSum.java
@@ -21,22 +21,46 @@ public class BinaryTreeMaximumPathSum {
 		      TreeNode right;
 		      TreeNode(int x) { val = x; }
 		  }
+	
+	int max = Integer.MIN_VALUE;
 	public int maxPathSum(TreeNode root) {
 	      
-        if (root == null)
+        maxPathSumInternal(root);
+        return this.max;
+    }
+	private int maxPathSumInternal(TreeNode root){
+		if (root == null)
         	return Integer.MIN_VALUE;
-        int left = maxPathSum(root.left);
-        int right = maxPathSum(root.right);
-        
-        int sumOf3Parts = (root.val == Integer.MIN_VALUE || left == Integer.MIN_VALUE || right == Integer.MIN_VALUE)? Integer.MIN_VALUE:root.val + left + right;
+        int left = maxPathSumInternal(root.left);
+        int right = maxPathSumInternal(root.right);
+        return setMax(root, left, right);
+	}
+	
+	private int setMax(TreeNode root, int left, int right){
+		int result = Integer.MIN_VALUE;
+		int sumOf3Parts = (root.val == Integer.MIN_VALUE || left == Integer.MIN_VALUE || right == Integer.MIN_VALUE)? Integer.MIN_VALUE:root.val + left + right;
         int sumOfLeftPath = (root.val == Integer.MIN_VALUE || left == Integer.MIN_VALUE)? Integer.MIN_VALUE:root.val + left;
         int sumOfRightPath = (root.val == Integer.MIN_VALUE || right == Integer.MIN_VALUE)? Integer.MIN_VALUE:root.val + right;
         
-        return Math.max(Math.max(root.val, sumOf3Parts), Math.max(sumOfLeftPath, sumOfRightPath));
+        int maxOfBranches = Math.max(Math.max(root.val, sumOf3Parts), Math.max(sumOfLeftPath, sumOfRightPath));
+        int maxOfLeaves = Math.max(left, right);
         
-    }
+        if (Math.max(maxOfBranches, maxOfLeaves) > this.max)
+        	this.max = Math.max(maxOfBranches, maxOfLeaves);
+        if (maxOfLeaves <= maxOfBranches)
+        	result = maxOfBranches;
+        return result;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BinaryTreeMaximumPathSum m = new BinaryTreeMaximumPathSum();
+		//build a binary tree from an array
+		TreeNode root = m.new TreeNode(-1);
+		root.right = m.new TreeNode(2);
+		root.left = m.new TreeNode(-2);
+		
+		System.out.println(m.maxPathSum(root));
 		
 
 	}
